cab5da9e213e2ab7866cf4485775e297272d4434
Abrahamjf <abrahamjferrer@gmail.com>
2022-06-30 17:28:11 +0000

    [ADD] Se modifcio el reporsitorio

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Jumbotron = () => {
+    return (
+         <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">A Warm Welcome</h1>
+            <p class="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam harum rerum voluptatibus magnam culpa nihil unde iste provident nam praesentium quia nisi quo quasi eligendi, consectetur velit quas maxime odio!</p>
+            <button class="btn btn-primary btn-lg" type="button">Call to Action!</button>
+        </div>
+    );
+};
+
+export default Jumbotron;
--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,16 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
-
-
+//import Home from "./component/home.jsx";
+import Navbar from "./component/navbar.jsx";
+import Jumbotron from "./component/jumbotron.jsx";
+import Card from "./component/card.jsx";
+//import Footer from "./component/footer";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector(""));
+//ReactDOM.render(<Home />, document.querySelector(""));
+ReactDOM.render(<Navbar />, document.querySelector("#navbar"));
+ReactDOM.render(<Jumbotron />, document.querySelector("#jumbotron"));
+ReactDOM.render(<Card />, document.querySelector("#cards"));
+
 
