1e26574cb878b1830f7640650bc216fa34acf8c2
Abrahamjf <abrahamjferrer@gmail.com>
2022-07-02 03:07:02 +0000

    [ADD] Se agrego contenido

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Rigobaby from "./img/rigo-baby.jpg"
+import image2 from "../../img/image2.jpg";
 
 
 export const Cards = () => {
     return (
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card m-4" style={{width: "18rem"}}>
+            <img src={image2} />
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
