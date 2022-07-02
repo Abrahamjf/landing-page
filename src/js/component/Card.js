f6f13dca19cc8e6f837bd883c9726e544f820e4c
Abrahamjf <abrahamjferrer@gmail.com>
2022-07-02 04:14:33 +0000

    [ADD] Se termino el proyecto

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,16 +1,28 @@
 import React from "react";
 import image2 from "../../img/image2.jpg";
+import PropTypes from "prop-types";
 
 
-export const Cards = () => {
+const orders = [1,2,3,4];
+
+export const Cards = ({...props}) => {
     return (
-        <div className="card m-4" style={{width: "18rem"}}>
-            <img src={image2} />
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    );
+        <div className= "card-group">
+            {orders.map((order,index) => {
+                return( <div key={index} className="card m-4">
+                            <img className="card-img-top" src={props.imgUrl= "https://picsum.photos/200/300?random"} alt="Imagen" /> 
+                            <div className="card-body">
+                                <h5 className="card-tittle">{props.tittle ="Card Title"}</h5>
+                                <p className="card-text">{props.description ="Some quick example text to build on the card title and make up the bulk of the card's content."}</p>
+                                <a href="#" className="btn btn-primary">Find Out More!</a>
+                            </div>
+                        </div>)
+            })}
+        </div>);
+};
+
+Cards.propTypes = {
+    imgUrl: PropTypes.any,
+    description: PropTypes.string,
+    tittle: PropTypes.string,
 };
